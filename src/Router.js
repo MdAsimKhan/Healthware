44000583f6be000ecd42914293d6a7372240e311
dashmat <dashmathembram99@gmail.com>
2023-03-26 23:55:49 +0530

    fixed dashboard

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,6 @@
 import "./css/App.css";
 import { Container } from "react-bootstrap";
-import Home from "./Home";
+import Home from "./home";
 import { Routes, Route } from "react-router-dom";
 import Navbars from "./components/Navbar/Navbars";
 import Signup from "./components/user_registration/signup_fr";
@@ -12,7 +12,9 @@ import DoctorDashboard from "./components/dashboard/doctor_dashboard";
 import PatientDashboard from "./components/dashboard/patient_dashboard";
 import Profile from "./components/dashboard/user_profile";
 import Appointment from "./components/dashboard/pages/book_doctor_appointment";
-
+import Dprofile from "./components/dashboard/dProfile";
+import HProfile from "./components/dashboard/hProfile";
+import HospitalDashboard from "./components/dashboard/hospital_dashboard"
 function App() {
   return (
     <div className="App">
@@ -39,8 +41,7 @@ function App() {
               <Route path="doctor_dashboard" element={<DoctorDashboard />} />
               <Route path="patient_dashboard" element={<PatientDashboard />} />
               <Route
-                path="hospital_dashboard"
-                element={<HospitalDashboard />}
+                path="hospital_dashboard" element={<HospitalDashboard />}
               />
             </Route>
 
