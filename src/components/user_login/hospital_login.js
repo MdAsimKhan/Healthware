356268671d4f30aa5862c97f5f5b73ffd4c197c1
Mohammad Asim Khan <ak4514768@gmail.com>
2023-03-30 19:22:47 +0530

    Updated hospital_login.js

--- src/components/user_login/hospital_login.js
+++ src/components/user_login/hospital_login.js
@@ -1,72 +1,59 @@
-import { useState } from 'react';
-// import { useHistory } from 'react-router-dom';
-import { Form, Button, Alert } from 'react-bootstrap';
-// import useFetch from 'react-fetch-hook';
-import { useNavigate } from 'react-router-dom';
-
-
-
+import { useState } from "react";
+import { Form, Button, Alert } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 function HospitalLogin() {
-  // const history = useHistory();
   const [validated, setValidated] = useState(false);
   const [formData, setFormData] = useState();
   const [showError, setShowError] = useState(false);
-  const [success, setSuccess] = useState(false)
+  const [success, setSuccess] = useState(false);
   const navigate = useNavigate();
-
-
 
   const handleChange = (event) => {
     setFormData({
-
       ...formData,
       [event.target.name]: event.target.value,
-
-
     });
   };
 
   const handleSubmit = async (event) => {
-
-
     event.preventDefault();
-
     // validate form
     const form = event.currentTarget;
-
     if (form.checkValidity() === false) {
       event.preventDefault();
       event.stopPropagation();
     }
-
     setValidated(true);
 
-    await fetch('http://localhost:3001/hospital').then(function (response) {
-      return response.json();
-    }).then(function (data) {
-      data.map(async (ele) => {
-        if (ele.email == formData.email) {
-          // console.log('Success!')
-
-          setSuccess(true);
-          // const Rd = 
-          // console.log(success)
-          { success ? navigate(`/${ele._id}/hospital_dashboard`) : console.log('not working') }
-        }
+    await fetch("http://localhost:3001/hospital")
+      .then(function (response) {
+        return response.json();
       })
-
-    })
-
-  }
+      .then(function (data) {
+        data.map(async (ele) => {
+          if (ele.email == formData.email) {
+            setSuccess(true);
+            {
+              success
+                ? navigate(`/${ele._id}/hospital_dashboard`)
+                : console.log("not working");
+            }
+          }
+        });
+      });
+  };
 
   return (
     <>
-
       <div className="container">
         <h1>Hospital login page</h1>
         {showError && (
-          <Alert variant="danger" onClose={() => setShowError(false)} dismissible>
+          <Alert
+            variant="danger"
+            onClose={() => setShowError(false)}
+            dismissible
+          >
             Invalid email or password
           </Alert>
         )}
@@ -83,14 +70,17 @@ function HospitalLogin() {
           </Form.Group>
           <Form.Group controlId="password">
             <Form.Label>Password</Form.Label>
-            <Form.Control name="password" onChange={handleChange} type="password" />
+            <Form.Control
+              name="password"
+              onChange={handleChange}
+              type="password"
+            />
           </Form.Group>
           <Button variant="primary" type="submit">
             Login
           </Button>
         </Form>
       </div>
-
     </>
   );
 }
